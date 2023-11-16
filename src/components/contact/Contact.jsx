cb56e80462d9913e03e0d2a0f99c312528401732
Mahesh Kumawat <mahesh.kumawat@marktine.com>
2023-11-16 17:41:45 +0530

    latest changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -57,15 +57,15 @@ const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hello@react.dev</span>
+          <span>mahikumawat121998@gmail.com</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Hello street New York</span>
+          <span>Rajasthan (332001) India</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
-          <span>+1 234 5678</span>
+          <span>+91 9828681407</span>
         </motion.div>
       </motion.div>
       <div className="formContainer">
