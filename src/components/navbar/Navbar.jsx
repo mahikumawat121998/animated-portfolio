afa1ff3d4e41c50b9ecdbaf40adfeea25bbcc66e
Mahesh Kumawat <mahesh.kumawat@marktine.com>
2023-11-16 12:56:45 +0530

    ci

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,27 +6,27 @@ const Navbar = () => {
   return (
     <div className="navbar">
       {/* Sidebar */}
-      <Sidebar/>
+      <Sidebar />
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          Lama Dev
+          DOTENV
         </motion.span>
         <div className="social">
           <a href="#">
-            <img src="/facebook.png" alt="" />
+            <img src="/facebook.png" alt="facebook.png" />
           </a>
-          <a href="#">
-            <img src="/instagram.png" alt="" />
+          <a href="https://www.instagram.com/deadofwrite_aksit/">
+            <img src="/instagram.png" alt="instagram.png" />
           </a>
-          <a href="#">
-            <img src="/youtube.png" alt="" />
+          <a href="https://www.quora.com/profile/Mahesh-Kumawat-178">
+            <img src="/quora.png" alt="quora.png" />
           </a>
-          <a href="#">
-            <img src="/dribbble.png" alt="" />
+          <a href="https://www.linkedin.com/in/mahesh-kumawat-68b0aa190/">
+            <img src="/linkdin.png" alt="linkdin.png" />
           </a>
         </div>
       </div>
